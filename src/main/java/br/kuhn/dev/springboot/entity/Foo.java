869724fc651634553af55975313764531fb9370d
Jardel <jardelkuhn@gmail.com>
2022-10-08 10:07:54 -0300

    fixed foo id annotation

--- src/main/java/br/kuhn/dev/springboot/entity/Foo.java
+++ src/main/java/br/kuhn/dev/springboot/entity/Foo.java
@@ -13,7 +13,7 @@ import javax.persistence.Version;
 public class Foo implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @Column(nullable = false)
